de30416db1c3e0a415a088b68efa36f70f6c6538
George Volokitin <saintgeo23@gmail.com>
2024-06-04 23:36:16 +0300

    GV-02. Added initial routing

---
+++ src/features/Account/Account.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Account.scss';
+
+function Account() {
+  return (
+    <div className="account">Account</div>
+  );
+}
+
+export default Account;
---
+++ src/features/Account/Register/Register.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Register.scss';
+
+function Register() {
+  return (
+    <div className="register">Register</div>
+  );
+}
+
+export default Register;
