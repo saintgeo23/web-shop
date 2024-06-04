002c3b5f15ddf7e04bac0fb9bd03fad0f0ffdacc
George Volokitin <saintgeo23@gmail.com>
2024-06-04 22:33:36 +0300

    GV-01. Initialized app, added scss & bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__card card">
+        <img
+          src="https://en.yoyostorerewind.com/cdn/shop/files/plasmacrash13_ea45495a-962e-416b-8606-a7df92c932e8.jpg?v=1716873682&width=400"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Plasma Crash</h5>
+          <p className="card-text">A variant of the Hydrogen Crash. Slightly smaller and with even more sleep.</p>
+          <a
+            href="https://en.yoyostorerewind.com/products/plasma-crash"
+            className="btn btn-primary"
+          >Go buy it</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
